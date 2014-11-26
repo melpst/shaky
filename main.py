431711d86e3f01beb44c8753ca73ac058fd416a7
Jitlada Supreeyaporn <melancholic.pastel.cnrl@gmail.com>
2014-11-26 22:21:24 +0700

    recommit main.py

--- main.py
+++ main.py
@@ -12,33 +12,39 @@ class ShakyGame(gamelib.SimpleGame):
     def __init__(self):
         super(ShakyGame, self).__init__('ShakyShaky', ShakyGame.BLACK)
         self.time = 0
+        self.music = Music()
+        
         self.up = Up()
         self.down = Down()
         self.left = Left()
         self.right = Right()
-        self.music = Music()
 
     def init(self):
         super(ShakyGame, self).init()
 
     def update(self):
         if self.is_key_pressed(K_UP):
-            self.player.move_up()
+            print "up"
         elif self.is_key_pressed(K_DOWN):
-            self.player.move_down()
+            print "down"
+        elif self.is_key_pressed(K_LEFT):
+            print "left"
+        elif self.is_key_pressed(K_RIGHT):
+            print "right"
         elif self.is_key_pressed(K_RETURN):
             self.is_started = True
-  #          self.music.play()
+#            self.music.play()
         if self.is_started:
             self.render_time()
         
     def render_time(self):
         self.time += self.clock.get_time()
-        self.time_image = self.font.render("Time = %d" % (self.time/1000.0), 0,ShakyGame.WHITE)
+        self.time_image = self.font.render("Time = %.3f" % (self.time/1000.0), 0,ShakyGame.WHITE)
 
     def render(self, surface):
         if self.is_started:
-            surface.blit(self.time_image, (480,10))
+            surface.blit(self.time_image, (800,10))
+
             self.up.render(surface)
             self.down.render(surface)
             self.left.render(surface)
